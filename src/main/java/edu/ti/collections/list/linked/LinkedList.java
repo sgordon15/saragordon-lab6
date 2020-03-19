d2b3766f2d95335a219e80c831f6a807260f557a
meahtc <55120690+meahtc@users.noreply.github.com>
2020-03-18 20:00:18 -0500

    Refactor Node to be a nested class of LinkedList add methods to be implemented add unit tests that should all pass when methods implmented correctly

--- src/main/java/edu/ti/collections/list/linked/LinkedList.java
+++ src/main/java/edu/ti/collections/list/linked/LinkedList.java
@@ -1,5 +1,80 @@
 package edu.ti.collections.list.linked;
 
 public class LinkedList {
+    private class Node {
+        Object payload;
+        Node next = null;
+
+        public Node(Object payload) {
+            this.payload = payload;
+        }
+
+        public Object getPayload() {
+            return payload;
+        }
+
+        public void setPayload(Object payload) {
+            this.payload = payload;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
     private Node head = null;
+
+    public LinkedList() {
+        // nothing
+    }
+
+    public LinkedList(Object payload) {
+        head = new Node(payload);
+    }
+
+    public boolean isEmpty() {
+        return (head == null);
+    }
+
+    //TODO -- implement private helper method end that returns last Node in list
+    private Node end() {
+        return null;
+    }
+
+    //TODO -- implement size to return number of Nodes in list
+    public int size() {
+        return -1;
+    }
+
+    //TODO -- implement insert, which inserts Node for object as new head of list
+    public void insert(Object object) {
+        //code needed here
+    }
+
+    //TODO -- implement append, that appends Node to end of list
+    public void append(Object object) {
+        //code needed here
+    }
+
+    //TODO -- implement get to retrieve the n-th object in the list,
+    //        return null if n > (size() - 1)
+    public Object get(int n) {
+        return null;
+    }
+
+    //TODO -- implement remove to remove n-th element of list,
+    //        return Object if n < size(), null otherwise
+    public Object remove(int n) {
+        return null;
+    }
+
+    //TODO -- implement remove to remove given object from list,
+    //        return Object if object is in the list, null otherwise
+    public Object remove(Object object) {
+        return null;
+    }
 }
--- src/test/java/edu/ti/collections/list/linked/LinkedListTest.java
+++ src/test/java/edu/ti/collections/list/linked/LinkedListTest.java
@@ -6,8 +6,90 @@ import static org.junit.Assert.*;
 
 public class LinkedListTest {
     @Test
-    public void testNewListIsEmpty() {
+    public void testNewEmptyListIsEmpty() {
         LinkedList linkedList = new LinkedList();
         assertTrue(linkedList.isEmpty());
+    }
+
+    @Test
+    public void testNewNotEmptyListIsNotEmpty() {
+        Integer i14 = new Integer(14);
+        LinkedList linkedList = new LinkedList(i14);
+        assertFalse(linkedList.isEmpty());
+    }
+
+    @Test
+    public void testInsertTwoObjectsGivesCorrectSize() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.insert(j37);
+        assertEquals(2, linkedList.size());
+    }
+
+    @Test
+    public void testAppendTwoObjectsGivesCorrectSize() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.append(j37);
+        assertEquals(2, linkedList.size());
+    }
+
+    @Test
+    public void testGetTooBigReturnsNull() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.append(j37);
+        assertNull(linkedList.get(2));
+    }
+
+    @Test
+    public void testInsertTwoObjectsCorrectOrder() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.insert(j37);
+        Integer first = (Integer) linkedList.get(0);
+        assertEquals(j37, first);
+        Integer second = (Integer) linkedList.get(1);
+        assertEquals(i12, second);
+    }
+
+    @Test
+    public void testAppendTwoObjectsCorrectOrder() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.append(j37);
+        Integer first = (Integer) linkedList.get(0);
+        assertEquals(i12, first);
+        Integer second = (Integer) linkedList.get(1);
+        assertEquals(j37, second);
+    }
+
+    @Test
+    public void testDeleteCorrectElementByPosition() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.insert(j37);
+        Integer actualInteger = (Integer) linkedList.remove(0);
+        assertEquals(j37, actualInteger);
+        assertEquals(1, linkedList.size());
+        assertEquals(i12, linkedList.get(0));
+    }
+
+    @Test
+    public void testDeleteCorrectElementByObject() {
+        Integer i12 = new Integer(12);
+        Integer j37 = new Integer(37);
+        LinkedList linkedList = new LinkedList(i12);
+        linkedList.insert(j37);
+        Integer actualInteger = (Integer) linkedList.remove(i12);
+        assertEquals(i12, actualInteger);
+        assertEquals(1, linkedList.size());
+        assertEquals(j37, linkedList.get(0));
     }
 }
